27013c466fb2903b837a0da10fe2cd42826e3539
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-07-28 16:38:11 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,7 +10,7 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -22,7 +22,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end 
   
   
   private
