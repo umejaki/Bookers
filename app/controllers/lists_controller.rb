e559d88a77a3541ce28d2ccd2e70e616dc0380f5
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-07-30 14:38:06 +0000

    [Finish] 3-15

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,19 +5,25 @@ class ListsController < ApplicationController
 
   # 以下を追加
   def create
+    @lists = List.all
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+   if @list.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
+   else
+      render :new
+   end
   end
 
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
+    
     @list = List.find(params[:id])
   end
 
@@ -26,15 +32,24 @@ class ListsController < ApplicationController
   end
   
   def update
-    list = List.find(params[:id])
-    list.update(list_params)
-    redirect_to list_path(list.id)
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+    redirect_to list_path(@list.id)
+    else
+      render :edit
+    end
   end 
   
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+     flash[:notice] = "Book was successfully destroyed."
+  end
   
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-end
+  end
