5da4f9f96b9b040f9d882dc3306b6f51704c8aa6
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-08-01 13:28:31 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@Book.id)
+      redirect_to book_path(@book.id)
    else
       render :index
    end
@@ -34,7 +34,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@Book.id)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
     else
       render :edit
     end
@@ -43,8 +44,8 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
-     flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path
+    flash[:notice] = "Book was successfully destroyed."
   end
   
   private
