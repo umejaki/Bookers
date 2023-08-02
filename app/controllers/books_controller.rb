aeb6c5f18ff302d4baef6011f3928f90364c6588
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-08-02 12:28:48 +0000

    [finish] 3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,7 +44,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
     flash[:notice] = "Book was successfully destroyed."
   end
   
